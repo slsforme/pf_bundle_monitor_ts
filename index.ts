f8c2515b2c61bf919216567e45a52989c4503072
slsforme <27ofdecember@gmail.com>
2025-02-24 23:56:11 +0300

    Refreshed index.ts

--- index.ts
+++ index.ts
@@ -5,17 +5,16 @@ import { PumpFunMonitor } from "./src/pumpfun/pumpFunMonitor";
 import { accountsMonitor, blacklistHandler } from "./src/accounts/accountsMonitor";
 import { tokenBuyMonitor } from "./src/pumpfun/tokenBuysMonitor";
 
-
 async function main() {
+  tokenMonitor.monitorTokens();
   accountsMonitor.monitorTasks();
   tokenBuyMonitor.monitorTasks();
   blacklistHandler.trackMatchMapChanges();
-  const pumpFunMonitor = new PumpFunMonitor(grpcUrl, tokenMonitor);
-  await pumpFunMonitor.startMonitoring()
+  const pumpFunMonitor = new PumpFunMonitor(tokenMonitor);
+  await pumpFunMonitor.startMonitoring();
 }
 
 main().catch((error) => {
   logger.error("Error in main():", error);
- process.exit(1);
+  process.exit(1);
 });
-
